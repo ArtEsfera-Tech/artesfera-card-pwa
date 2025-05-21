f0ae5fabf9925b2833afd22857ab8352c65c1f05
Daedaluzz <arthurbernardpereira@gmail.com>
2025-05-21 02:17:36 -0300

    Chore: little adjustment on images on manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,11 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
         src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
       },
       {
         src: "/favicon-16x16.png",
